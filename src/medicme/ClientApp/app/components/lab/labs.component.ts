ed15c1a0331e5ecb6a725b7154a1a0e9fdb36e29
zohra <zohra@DESKTOP-ADNSG9A>
2017-03-20 12:09:08 +0100

    Fixed labs navigation

---
+++ src/medicme/ClientApp/app/components/lab/labs.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+import { fadeInOut } from '../../services/animations';
+
+@Component({
+    selector: 'labs',
+    templateUrl: './labs.component.html',
+    styleUrls: ['./labs.component.css'],
+    animations: [fadeInOut]
+})
+export class labsComponent {
+}
